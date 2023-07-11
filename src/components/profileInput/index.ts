11e3681213a387eb9f8c8f38f3b0f3d5e33284df
Egor Talovikov <egtalovikov@gmail.com>
2023-07-11 18:21:47 +0300

    hotfix: убрал лишний пробел

--- src/components/profileInput/index.ts
+++ src/components/profileInput/index.ts
@@ -87,4 +87,3 @@ export default class profileInput extends Block {
 Handlebars.registerHelper('ifEquals', function(this: any, arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
   });
-  
